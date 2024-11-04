30e672f9a16cff77c230a54808aebaee244029b2
lovelyfirefly <1494932845@qq.com>
2024-11-04 21:50:53 +0800

    额外夹板强化

---
+++ src/main/java/com/hoshino/cti/Modifier/ararar.java
@@ -0,0 +1,21 @@
+package com.hoshino.cti.Modifier;
+
+import com.marth7th.solidarytinker.extend.superclass.ArmorModifier;
+import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.tools.nbt.IToolContext;
+import slimeknights.tconstruct.library.tools.stat.ModifierStatsBuilder;
+import slimeknights.tconstruct.library.tools.stat.ToolStats;
+
+public class ararar extends ArmorModifier {
+    @Override
+    public boolean havenolevel() {
+        return true;
+    }
+
+    @Override
+    public void addToolStats(IToolContext context, ModifierEntry modifier, ModifierStatsBuilder builder) {
+        ToolStats.DURABILITY.add(builder,400 * modifier.getLevel());
+        ToolStats.ARMOR_TOUGHNESS.add(builder,2*modifier.getLevel());
+        ToolStats.ARMOR.add(builder,3*modifier.getLevel());
+    }
+}
