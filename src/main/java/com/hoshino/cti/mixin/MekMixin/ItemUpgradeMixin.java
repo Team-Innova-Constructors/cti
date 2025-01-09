76d8f77209d79203a65fd911c0e96892fd3e2878
EtSH-C2H6S <2130679254@qq.com>
2025-01-09 12:56:52 +0800

    mek插件bug修复

--- src/main/java/com/hoshino/cti/mixin/MekMixin/ItemUpgradeMixin.java
+++ src/main/java/com/hoshino/cti/mixin/MekMixin/ItemUpgradeMixin.java
@@ -1,14 +1,57 @@
 package com.hoshino.cti.mixin.MekMixin;
 
+import mekanism.api.Upgrade;
 import mekanism.common.item.ItemUpgrade;
+import mekanism.common.tile.component.TileComponentUpgrade;
+import mekanism.common.tile.interfaces.IUpgradeTile;
+import mekanism.common.util.WorldUtils;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(remap = false,value = ItemUpgrade.class)
 public class ItemUpgradeMixin {
-    @ModifyArg(method = "m_6225_",at = @At(value = "INVOKE", target = "Lmekanism/common/tile/component/TileComponentUpgrade;addUpgrades(Lmekanism/api/Upgrade;I)I"),index = 1)
-    protected int limitCount(int maxAvailable){
-        return Math.min(maxAvailable, 8);
+    /**
+     * @author EtSH_C2H6S
+     * @reason 限制插件插入数量
+     */
+    @Overwrite
+    public @NotNull InteractionResult m_6225_(UseOnContext context) {
+        ItemUpgrade itemUpgrade =(ItemUpgrade)(Object) this;
+        Player player = context.getPlayer();
+        if (player != null && player.isShiftKeyDown()) {
+            Level world = context.getLevel();
+            BlockEntity tile = WorldUtils.getTileEntity(world, context.getClickedPos());
+            if (tile instanceof IUpgradeTile upgradeTile) {
+                if (upgradeTile.supportsUpgrades()) {
+                    TileComponentUpgrade component = upgradeTile.getComponent();
+                    ItemStack stack = context.getItemInHand();
+                    Upgrade type = itemUpgrade.getUpgradeType(stack);
+                    if (component.supports(type)) {
+                        if (!world.isClientSide) {
+                            int toAdd = Math.min(8-component.getUpgrades(type),stack.getCount());
+                            if (toAdd>0&&toAdd<=8) {
+                                int added = component.addUpgrades(type, toAdd);
+                                if (added > 0) {
+                                    stack.shrink(added);
+                                }
+                            }
+                        }
+
+                        return InteractionResult.sidedSuccess(world.isClientSide);
+                    }
+                }
+            }
+        }
+
+        return InteractionResult.PASS;
     }
 }
