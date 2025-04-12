f2db3b2c2f469d80e09fabf26f52902003f6c96b
lovelyFirefly <1494932845@qq.com>
2025-04-12 17:42:54 +0800

    增强诅咒

--- src/main/java/com/hoshino/cti/Event/CommonLivingHurt.java
+++ src/main/java/com/hoshino/cti/Event/CommonLivingHurt.java
@@ -1,23 +1,36 @@
 package com.hoshino.cti.Event;
 
 import com.hoshino.cti.register.ctiEffects;
+import com.hoshino.cti.register.ctiHostilityTrait;
 import com.hoshino.cti.register.ctiModifiers;
 import com.hoshino.cti.util.method.GetModifierLevel;
+import dev.xkmc.l2hostility.content.capability.mob.MobTraitCap;
+import dev.xkmc.l2hostility.content.traits.base.MobTrait;
+import dev.xkmc.l2hostility.init.registrate.LHTraits;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.AABB;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingHealEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.level.ExplosionEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.util.List;
+import java.util.Set;
 
 import static com.hoshino.cti.cti.MOD_ID;
 
@@ -42,6 +55,55 @@ public class CommonLivingHurt {
                         event.setCanceled(true);
                     }
                 }
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGH)
+    public static void whenCurseMobAttackPlayer(LivingHurtEvent event) {
+        if (event.getSource().getEntity() instanceof Mob mob) {
+            LazyOptional<MobTraitCap> optional = mob.getCapability(MobTraitCap.CAPABILITY);
+            if (optional.resolve().isPresent()) {
+                MobTraitCap cap = optional.resolve().get();
+                Set<MobTrait> set = cap.traits.keySet();
+                for (int i = 0; i < set.stream().toList().size(); i++) {
+                    MobTrait trait = LHTraits.CURSED.get();
+                    if (cap.hasTrait(trait) && event.getEntity() instanceof Player player) {
+                        int a = player.getPersistentData().getInt("pain");
+                        player.getPersistentData().putInt("pain", Math.min(a + 1, 12));
+                    }
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void curseCostPlayerHeal(LivingHealEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            int a = player.getPersistentData().getInt("pain");
+            float shouldHeal = event.getAmount();
+            if (a > 0) {
+                switch (a) {
+                    case 1, 2, 3 -> {
+                        event.setAmount(Math.max(0.1F, shouldHeal - a * 0.7F));
+                    }
+                    case 4, 5, 6 -> {
+                        event.setAmount(Math.max(0.05F, shouldHeal - a * 0.6F));
+                    }
+                    case 7, 8, 9 -> {
+                        event.setAmount(Math.max(0, shouldHeal - a * 0.5F));
+                    }
+                }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void livingTick(LivingEvent.LivingTickEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            int a = player.getPersistentData().getInt("pain");
+            if (a > 0 && player.tickCount % 160 == 0) {
+                player.getPersistentData().putInt("pain", a - 1);
             }
         }
     }
