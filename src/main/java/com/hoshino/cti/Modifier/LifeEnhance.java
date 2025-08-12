704f5e46b1bc27eda0ab38c10e759a22e5c6bad0
EtSH-C2H6S <2130679254@qq.com>
2025-08-12 21:08:35 +0800

    修复生命强化不生效

--- src/main/java/com/hoshino/cti/Modifier/LifeEnhance.java
+++ src/main/java/com/hoshino/cti/Modifier/LifeEnhance.java
@@ -7,12 +7,20 @@ import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.ModifierHooks;
 import slimeknights.tconstruct.library.modifiers.hook.behavior.AttributesModifierHook;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
 import java.util.function.BiConsumer;
 
 public class LifeEnhance extends EtSTBaseModifier implements AttributesModifierHook {
+    @Override
+    protected void registerHooks(ModuleHookMap.Builder builder) {
+        super.registerHooks(builder);
+        builder.addHook(this, ModifierHooks.ATTRIBUTES);
+    }
+
     @Override
     public void addAttributes(IToolStackView tool, ModifierEntry modifier, EquipmentSlot slot, BiConsumer<Attribute, AttributeModifier> consumer) {
         consumer.accept(Attributes.MAX_HEALTH,new AttributeModifier(CommonUtil.UUIDFromSlot(slot,modifier.getId()),Attributes.MAX_HEALTH.getDescriptionId(),20, AttributeModifier.Operation.ADDITION));
